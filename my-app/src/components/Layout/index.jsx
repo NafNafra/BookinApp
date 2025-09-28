475e186ee6638a1e80efcb275e679b1897d1921b
NafNafra <nafafyandrianaivo@gmail.com>
2025-09-28 15:59:56 +0300

    remove(SidePanel): remove

--- my-app/src/components/Layout/index.jsx
+++ my-app/src/components/Layout/index.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
       <div className="layout">
 
-        <SidePanel />
+        {/* <SidePanel /> */}
         <div>
           <Outlet />
         </div>
