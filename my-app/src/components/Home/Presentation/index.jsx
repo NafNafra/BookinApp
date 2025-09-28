629b8b91beae1430f1ad37c6593d3fd8ef23c944
NafNafra <nafafyandrianaivo@gmail.com>
2025-09-28 21:20:55 +0300

    add(Presentation)

--- my-app/src/components/Home/Presentation/index.jsx
+++ my-app/src/components/Home/Presentation/index.jsx
@@ -1,7 +1,58 @@
+import './Presentation.scss'
+
 const Present = () => {
-  return(
+  return (
     <div className="Presentation">
-      Presentation
+      <div className="destination">
+        <div className="intro">Decouvrer votre destination</div>
+        <div className="texte">Explorer differents types de proprietes de differents lieux de votre preference</div>
+      </div>
+      <div className="cartes">
+        <div className="carte">
+          <div className="titre">
+            <div className='description'>
+              <h3>Villa</h3>
+              <div><div className="lieu-icon" /><div className="valabilite">12.891 valables</div></div>
+            </div>
+            <div className="voir-plus" />
+          </div>
+          <div className="lieux-image"></div>
+        </div>
+
+        <div className="carte">
+          <div className="titre">
+            <div className='description'>
+              <h3>Villa</h3>
+              <div><div className="lieu-icon" /><div className="valabilite">12.891 valables</div></div>
+            </div>
+            <div className="voir-plus" />
+          </div>
+          <div className="lieux-image"></div>
+        </div>
+
+        <div className="carte">
+          <div className="titre">
+            <div className='description'>
+              <h3>Villa</h3>
+              <div><div className="lieu-icon" /><div className="valabilite">12.891 valables</div></div>
+            </div>
+            <div className="voir-plus" />
+          </div>
+          <div className="lieux-image"></div>
+        </div>
+
+        <div className="carte">
+          <div className="titre">
+            <div className='description'>
+              <h3>Villa</h3>
+              <div><div className="lieu-icon" /><div className="valabilite">12.891 valables</div></div>
+            </div>
+            <div className="voir-plus" />
+          </div>
+          <div className="lieux-image"></div>
+        </div>
+
+      </div>
     </div>
   )
 }
