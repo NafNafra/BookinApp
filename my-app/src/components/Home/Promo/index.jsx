2affef7fcc69d56bad6fa7492fc0a5ba80f5d7b9
NafNafra <nafafyandrianaivo@gmail.com>
2025-09-29 20:16:48 +0300

    add(Promo): content

--- my-app/src/components/Home/Promo/index.jsx
+++ my-app/src/components/Home/Promo/index.jsx
@@ -1,3 +1,5 @@
+import './Promo.scss'
+
 const Promo = () => {
   return (
     <div className="promo">
@@ -6,8 +8,27 @@ const Promo = () => {
         <div className="voir"><div>Voir tout</div><div className="voir-tout" /></div>
       </div>
       <div className="promos">
-        
+        <div className="carte">
+          <div className="info">
+            <div className="rank"></div>
+            <div className="part-1">Gagnez des reductions extras pour la location dans l'hotel Azure</div>
+            <div className="part-2">50 %</div>
+            <div className="part-3">avec termes et conditions</div>
+          </div>
+          <div className="duree"> Valide du 14 au 20 Octobre 2025</div>
+        </div>
+
+        <div className='carte'>
+          <div className="info">
+            <div className="rank"></div>
+            <div className="part-1">Gagnez des reductions extras pour la location dans l'hotel Azure</div>
+            <div className="part-2">50 %</div>
+            <div className="part-3">avec termes et conditions</div>
+          </div>
+          <div className="duree"> Valide du 14 au 20 Octobre 2025</div>
+        </div>
       </div>
+
     </div>
   )
 }
