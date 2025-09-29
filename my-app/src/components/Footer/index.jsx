09a6751236cae37c9ed79dae0e3afd6a24fdbb1f
NafNafra <nafafyandrianaivo@gmail.com>
2025-09-29 20:35:33 +0300

    add(Footer)

---
+++ my-app/src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <div className="info">
+        </div>
+        <div className="about-support"></div>
+        <div className="contact"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
