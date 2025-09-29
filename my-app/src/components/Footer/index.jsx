a0924f59b0f14efce8f022da2b5d13746211f5a9
NafNafra <nafafyandrianaivo@gmail.com>
2025-09-29 21:35:03 +0300

    feat(Footer)

--- my-app/src/components/Footer/index.jsx
+++ my-app/src/components/Footer/index.jsx
@@ -3,9 +3,10 @@ const Footer = () => {
     <div className="footer">
       <div>
         <div className="info">
+          About this app and project
         </div>
-        <div className="about-support"></div>
-        <div className="contact"></div>
+        <div className="about-support">About and Support</div>
+        <div className="contact">Contact</div>
       </div>
     </div>
   )
