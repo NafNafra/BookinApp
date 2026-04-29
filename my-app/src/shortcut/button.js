84fadf213ab8e52f84655a4e458c4aa11f95337b
NafNafra <nafafyandrianaivo@gmail.com>
2026-04-29 16:41:38 +0300

    reset to nextjs

--- my-app/src/shortcut/button.js
+++
@@ -1,9 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-export default function Boutons() {
-  const navigate = useNavigate();
-
-  return (
-    <div><button>Book</button><button onClick={() => { navigate('/booking') }}>Book</button></div>
-  )
-}
